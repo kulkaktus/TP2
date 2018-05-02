6ac0fc4eb02e34315a413cb167579f6abef0e88f
kulkaktus <davidhw@stud.ntnu.no>
2018-05-02 10:53:47 +0200

    .

--- Ex2_2.m
+++ Ex2_2.m
@@ -1,12 +1,23 @@
 load('TorMod.mat');
 A = G3.f;
 B = G3.b;
-
-p1 = -0.5;
-p2 = 0.5;
-P = [1 p1];
+d = [0 0 1];
+p1 = 0.7939;
+p2 = -1.7689;
+P = [1 p1 p2];
 
 Hs = [1 -1];
-Hr = [1];
+Hr = [1]; 
 
-[R, S] = poleplace(B, A, Hr, Hs, P)
+[R, S] = poleplace(B, A, Hr, Hs, P) 
+
+P_end = conv(A,S) + conv(B,R)
+T = sum(R)
+
+
+%% Ex2_3
+CL = tf(conv(T,B), P_end, Ts,'variable','z^-1')
+step(CL)
+
+K = deconv(R,S); %Comment on the result
+U=tf(conv(A,R), P, Ts, 'variable', 'z^-1');
--- poleplace.m
+++ poleplace.m
@@ -1,7 +1,11 @@
 function [R,S]=poleplace(B,A,Hr,Hs,P)
-    Ts = 0.04;
-    d = 2;
+    d = 0;
     
+    for i = 2:length(B)
+        if B(i) == 0
+            d = d + 1
+        end
+    end
     n_a = length(A)-1;
     n_b = length(B);
     n_hr = length(Hr)-1;
@@ -11,23 +15,16 @@ function [R,S]=poleplace(B,A,Hr,Hs,P)
     
     n_s1 = n_b+n_hr+d-1;
     
-    %Convert to TFs
-    A = tf(A, 1, Ts, 'Variable', 'q^-1');
-    B = tf(B, 1, Ts, 'Variable', 'q^-1');
-    Hr = tf(Hr, 1, Ts, 'Variable', 'q^-1');
-    Hs = tf(Hs, 1, Ts, 'Variable', 'q^-1');
-    
-    A1 = A*Hs;
-    B1 = B*Hr;
-    
-    A1_coefs = cell2mat(A1.numerator);
-    B1_coefs = cell2mat(B1.numerator);
+
+    A1 = conv(A,Hs);
+    B1 = conv(B,Hr);
     
     % M1, M2 are left and right halves of sylvester matrix
-    M1 = tril(toeplitz([A1_coefs,zeros(1,n_b1+d-1)]));
+    M1 = tril(toeplitz([A1,zeros(1,n_b1+d-1)]));
     M1 = M1(:, 1:n_b1+d);
-    M2 = tril(toeplitz([zeros(1,n_a1+d-n_b1+1), B1_coefs, zeros(1,n_b1-1)]));
+    M2 = tril(toeplitz([zeros(1,n_a1+d-n_b1), B1, zeros(1,n_b1)]));
     M2 = M2(:, 1:n_a1);
+    
     M = [M1, M2];
     p = ([P,zeros(1, n_a1+n_b1+d-length(P))])';
     x = M\p;
@@ -35,12 +32,7 @@ function [R,S]=poleplace(B,A,Hr,Hs,P)
     S1 = x(1:n_s1+1)';
     R1 = x(n_s1+2:end)';
     
-    S1 = tf(S1, 1, Ts, 'Variable', 'q^-1');
-    R1 = tf(R1, 1, Ts, 'Variable', 'q^-1');
-    
-    S = S1*Hs;
-    R = R1*Hr;
-    
-    S = cell2mat(S.numerator);
-    R = cell2mat(R.numerator);
+    S = conv(S1,Hs);
+    R = conv(R1,Hr);
+   
 end
