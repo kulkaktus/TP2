e7a224b27ca87490022247c9b1d8832cb0ad3f5e
Unknown <sten_elling@hotmail.com>
2018-05-16 10:30:22 +0200

    hei

--- Ex2_2.m
+++ Ex2_2.m
@@ -1,14 +1,11 @@
+%% 2.2
 load('TorMod.mat');
 A = G3.f;
 B = G3.b;
-d = [0 0 1];
-p1 = 0.7939;
-p2 = -1.7689;
+p1 = -1.7689;
+p2 = 0.7939;
 P = [1 p1 p2];
 
-p1 = -0.5;
-p2 = 0.5;
-P = [1 p1];
 
 Hs = [1 -1];
 Hr = [1]; 
@@ -20,8 +17,43 @@ T = sum(R)
 
 
 %% Ex2_3
-CL = tf(conv(T,B), P, Ts,'variable','z^-1')
+Ts = 0.04;
+CL = tf(conv(T,B), P_end, Ts,'variable','z^-1')
+figure(1)
 step(CL)
+stepinfo(CL)
 
-K = deconv(R,S); %Comment on the result
+K = tf(R,S, Ts,'variable','z^-1')
+OL = G3*K;
+figure(2)
+nyquist(OL) % 3 encriclments around -1 corresponds to 3 unstable poles 
+
 U=tf(conv(A,R), P, Ts, 'variable', 'z^-1');
+figure(3)
+step(U)% Does not fullfill the criterium
+figure(4)
+bode(U)
+
+S3 = feedback(1,K*G3);
+M_m3 =inv(norm(S3,inf)) % 0.1566
+%% 2.4
+load('TorMod.mat');
+A = G3.f;
+B = G3.b;
+p1 = -1.7689;
+p2 = 0.7939;
+P = [1 p1 p2];
+
+q = tf('q', 0.04);
+
+Hs = [1 -1];
+Hr = [1 1]; 
+
+[R, S] = poleplace(B, A, Hr, Hs, P) 
+
+P_end = conv(A,S) + conv(B,R)
+
+
+
+T = sum(R)
+
--- poleplace.m
+++ poleplace.m
@@ -8,19 +8,19 @@ function [R,S]=poleplace(B,A,Hr,Hs,P)
     n_a1 = n_a+n_hs
     n_b1 = n_b+n_hr
     
-    n_s1 = n_b+n_hs+d-1
+    n_s1 = n_b+n_hr+d-1
 
     A1 = conv(A,Hs)
-    B1 = conv([0 1],conv(B,Hr))
-    
+    B1 = conv(B,Hr)
+    n_a1+d-n_b1-n_hr+1
     % M1, M2 are left and right halves of sylvester matrix
     M1 = tril(toeplitz([A1,zeros(1,n_b1+d-1)]));
     M1 = M1(:, 1:n_b1+d)
-    M2 = tril(toeplitz([zeros(1,n_a1+d-2*n_b1), B1, zeros(1,n_b1+2)]));
+    M2 = tril(toeplitz([zeros(1,n_a1+d-n_b1-n_hr-2), B1(2:end), zeros(1,n_b1-d+3)]));
     M2 = M2(:, 1:n_a1)
     
-    M = [M1, M2];
-    p = ([P,zeros(1, n_a1+n_b1+d-length(P))])';
+    M = [M1, M2]
+    p = ([P,zeros(1, n_a1+n_b1+d-length(P))])'
     x = M\p;
  
     S1 = x(1:n_s1+1)'
@@ -32,5 +32,6 @@ function [R,S]=poleplace(B,A,Hr,Hs,P)
     
     S = conv(S1,Hs);
     R = conv(R1,Hr);
-   
+ 
+ 
 end
