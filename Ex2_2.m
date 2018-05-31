6f54ea9e143ab501850e88256029e902a06a65b0
kulkaktus <davidhw@stud.ntnu.no>
2018-05-31 19:09:39 +0200

    Output sensitivity function should be correct
    Previously  confusing S in RST with S in sensitivity function

--- Ex2_2.m
+++ Ex2_2.m
@@ -2,17 +2,17 @@
 load('TorMod.mat');
 A = G3.f;
 B = G3.b;
-p1 = -1.7689;
+p1 = -1.7689; 
 p2 = 0.7939;
 P = [1 p1 p2];
 
 
 Hs = [1 -1];
 Hr = [1]; 
-[R, S] = poleplace(B, A, Hr, Hs, P);
+[R_old, S_old] = poleplace(B, A, Hr, Hs, P);
 
-P_end = conv(A,S) + conv(B,R);
-T = sum(R)
+P_end_old = conv(A,S) + conv(B,R);
+T_old = sum(R)
 
 
 %% Ex2_3
@@ -40,82 +40,108 @@ S3 = feedback(1,K*G3);
 M_m3 =inv(norm(S3,inf)) % 0.1566
 %% 2.4
 load('TorMod.mat');
+Ts = 0.04;
 A = G3.f;
 B = G3.b;
-p1 = -1.7689;
+p1 = -1.7689; 
 p2 = 0.7939;
 P = [1 p1 p2];
 
-q = tf('q', 0.04);
-
 Hs = [1 -1];
 Hr = [1 1]; 
-a = 0.3;
-peaks = zeros(20, 3);
-current_best = 100;
-i = 1
-for nq = 9
-    for a=0.4
-        B = G3.b;
+a = 0.2;
+nq = 9;
+
+
+Margins = zeros(50, 3);
+index = 1;
+for a = 0.1:0.05:0.5
+    for nq = 7:10
+        B = G3.B;
         poles_aux = [a,a,a,a,a,a,a,a,a,a];
         coefs = poly(poles_aux);
         P_new = conv(P, coefs);
 
         [R, S] = poleplace(B, A, Hr, Hs, P_new) ;
+        P_end = conv(A,S) + conv(B,R);
 
-        P_end = conv(A,S) + conv(B,R)
+        B = B(2:end); %Separate delay and B
+        q_delay = [0 1];
 
-        T = sum(R);
-
-
-        Ts = 0.04;
-        CL = tf(conv(T,B), P_end, Ts,'variable','z^-1'); 
-        figure(1)
-        step(CL)
-        U=tf(conv(A,R), P_end, Ts, 'variable', 'z^-1');
-        figure(3)
-        bodemag(U)% Does not fullfill the criterium
-        figure(5)
-        B = B(2:end);
         M_m = 0.4;
         U_max = 56.2; %35 dB  = 56.2
-        q_delay = [0 1];
 
         R = [R, zeros(1, nq)];
         S = [S, zeros(1, nq)];
-        new_R = @(Q) R + conv(A, conv(Hr, conv(Hs, Q)));
-        new_S = @(Q) (S - conv(q_delay, conv(B, conv(Hs, conv(Hr, Q)))));
 
-        c = @(Q) [norm(M_m*(S - conv(q_delay, conv(B, conv(Hs, conv(Hr, Q))))), Inf) - 1;
-             norm(tf(conv(A, new_R(Q)), P_end, Ts, 'variable', 'z^-1'), Inf) - U_max];
+        R_new = @(Q) R + conv(A, conv(Hr, conv(Hs, Q)));
+        S_new = @(Q) (S - conv(q_delay, conv(B, conv(Hs, conv(Hr, Q)))));
+        
+        K_new = @(Q) tf(R_new(Q),S_new(Q), Ts,'variable','z^-1');
+        output_sensitivity = @(Q) feedback(1,K_new(Q)*G3);
+        Mod_marg = @(Q) -norm(output_sensitivity(Q), Inf)^(-1);
+
+        %Set inequality (c) and equality (ceq) constraints
+        c = @(Q) [norm(M_m*output_sensitivity(Q), Inf) - 1;
+             norm(tf(conv(A, R_new(Q)), P_end, Ts, 'variable', 'z^-1'), Inf) - U_max];
         ceq = @(Q) [];
-        zero = @(Q) 0;
-        Mod_marg = @(Q) norm(tf(S + conv(q_delay, conv(B, conv(Hs, conv(Hr, Q)))), 1 ,Ts, 'variable', 'z^-1'), Inf)^(-1);
         Nonlincon = @(Q)deal(c(Q), ceq(Q));
-        Q_opt = fmincon(Mod_marg, zeros(1, nq), [], [], [],[], [-Inf,-Inf], [Inf,Inf], Nonlincon);
-
-        final_R = new_R(Q_opt);
-        final_S = new_S(Q_opt);
-
-        T = sum(final_R);
 
 
-        Ts = 0.04;
+        Q_opt = fmincon(Mod_marg, zeros(1, nq),[],[],[],[],[-Inf,-Inf],[Inf,Inf], Nonlincon);
 
-         CL = tf(conv(T,G3.b), P_end, Ts,'variable','z^-1'); 
-         U=tf(conv(A,final_R), P_end, Ts, 'variable', 'z^-1');
-         bode(U)
-         input= tf(conv(T,A), conv(A,final_S) + conv(conv(q_delay, B), final_R), Ts,'variable','z^-1');
-         step(input)
+        R_final = R_new(Q_opt);
+        S_final = S_new(Q_opt);
+        T = sum(R_final);
 
-    %     figure(11)
-
-        aaa = stepinfo(CL);
-       
-       peaks(i,:) = [nq, a, aaa.RiseTime];
-        i = i + 1;
+        K_final = tf(R_final,S_final, Ts,'variable','z^-1');
+        Sens_out_new = feedback(1,K_final*G3);
+        MM_new = -Mod_marg(Q_opt)
+        Margins(index, :) = [MM_new, a, nq];
+        index = index + 1
     end
 end
-input = tf(conv(T,A), conv(A,S) + conv(conv(q_delay, B), R), Ts,'variable','z^-1');
-current_best
-peaks
+
+%% Check constraints
+CL = tf(conv(T,G3.b), P_end, Ts,'variable','z^-1'); 
+U=tf(conv(A,R_final), P_end, Ts, 'variable', 'z^-1');
+
+input= tf(conv(T,A), conv(A,S_final) + conv(conv(q_delay, B), R_final), Ts,'variable','z^-1'); figure(1);  
+step(input); figure(2);
+stepinfo(CL) 
+figure(3);
+bodemag(U);
+
+
+%% Plots
+hold on
+
+subplot(2,2,1)
+CL_old = tf(conv(T_old,G3.b), P_end_old, Ts,'variable','z^-1'); 
+CL = tf(conv(T,G3.b), P_end, Ts,'variable','z^-1'); 
+title("Tracking step response, original and improved controller")
+xlabel("Time [s]")
+legend("Original", "Improved")
+step(CL_old)
+step(CL)
+
+subplot(2,2,2)
+B = G3.B;
+B = B(2:end);
+input_old = tf(conv(T_old,A), conv(A,S_old) + conv(conv(q_delay, B), R_old), Ts,'variable','z^-1'); figure(1);  
+input= tf(conv(T,A), conv(A,S_final) + conv(conv(q_delay, B), R_final), Ts,'variable','z^-1'); figure(1);  
+title("Control signal, original and improved controller")
+xlabel("Time [s]")
+ylabel("u")
+legend("Original", "Improved")
+step(input_old)
+step(input)
+
+subplot(2,2,3)
+Sens_out = feedback(1,K*G3);
+K_final = tf(R_final,S_final, Ts,'variable','z^-1');
+Sens_out_new = feedback(1,K_final*G3);
+MM_old = norm(Sens_out, Inf)^-1
+MM_new = (norm(Sens_out_new, Inf))^(-1)
+
+subplot(2,2,4)
