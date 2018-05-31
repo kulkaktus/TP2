e11b301f5d658d7108f93b4d0bd44009a01c3394
kulkaktus <davidhw@stud.ntnu.no>
2018-05-31 22:55:10 +0200

    vær obs på rare resultater, har endra en del variabelnavn

--- Ex2_2.m
+++ Ex2_2.m
@@ -11,26 +11,26 @@ Hs = [1 -1];
 Hr = [1]; 
 [R_old, S_old] = poleplace(B, A, Hr, Hs, P);
 
-P_end_old = conv(A,S) + conv(B,R);
-T_old = sum(R)
+P_end_old = conv(A,S_old) + conv(B,R_old);
+T_old = sum(R_old)
 
 
 %% Ex2_3
 Ts = 0.04;
-CL = tf(conv(T,B), P_end, Ts,'variable','z^-1')
-figure(1)
+CL = tf(conv(T_old,B), P_end_old, Ts,'variable','z^-1')
+figure(2)
 step(CL)
 stepinfo(CL)
 
 K = tf(R,S, Ts,'variable','z^-1')
 OL = tf(G3*K);
-figure(2)
-nyquist(OL) % 3 encirclements around -1 corresponds to 3 unstable poles 
+%figure(2)
+%nyquist(OL) % 3 encirclements around -1 corresponds to 3 unstable poles 
 
 
 
 figure(3)
-input = tf(conv(T,A), conv(A,S) + conv(B, R), Ts,'variable','z^-1');
+input_old = tf(conv(T_old,A), conv(A,S_old) + conv(B, R_old), Ts,'variable','z^-1');
 step(input)
 figure(4)
 U=tf(conv(A,R), P_end, Ts, 'variable', 'z^-1');
@@ -53,10 +53,11 @@ a = 0.2;
 nq = 9;
 
 
-Margins = zeros(50, 3);
-index = 1;
-for a = 0.1:0.05:0.5
-    for nq = 7:10
+Margins = zeros(1, 3);
+hail_marys = zeros(1, 4); %These satisfy all constraints and specs
+index = 1;  
+for a = 0.16
+    for nq = 8
         B = G3.B;
         poles_aux = [a,a,a,a,a,a,a,a,a,a];
         coefs = poly(poles_aux);
@@ -79,7 +80,7 @@ for a = 0.1:0.05:0.5
         
         K_new = @(Q) tf(R_new(Q),S_new(Q), Ts,'variable','z^-1');
         output_sensitivity = @(Q) feedback(1,K_new(Q)*G3);
-        Mod_marg = @(Q) -norm(output_sensitivity(Q), Inf)^(-1);
+        Mod_marg = @(Q) norm(output_sensitivity(Q), Inf)^(-1);
 
         %Set inequality (c) and equality (ceq) constraints
         c = @(Q) [norm(M_m*output_sensitivity(Q), Inf) - 1;
@@ -96,46 +97,49 @@ for a = 0.1:0.05:0.5
 
         K_final = tf(R_final,S_final, Ts,'variable','z^-1');
         Sens_out_new = feedback(1,K_final*G3);
-        MM_new = -Mod_marg(Q_opt)
+        MM_new = Mod_marg(Q_opt)
+     
         Margins(index, :) = [MM_new, a, nq];
         index = index + 1
+        CL = tf(conv(T,G3.b), P_end, Ts,'variable','z^-1'); 
+        cake = stepinfo(CL)
     end
 end
-
+hail_marys
 %% Check constraints
 CL = tf(conv(T,G3.b), P_end, Ts,'variable','z^-1'); 
 U=tf(conv(A,R_final), P_end, Ts, 'variable', 'z^-1');
 
 input= tf(conv(T,A), conv(A,S_final) + conv(conv(q_delay, B), R_final), Ts,'variable','z^-1'); figure(1);  
-step(input); figure(2);
+step(input); 
 stepinfo(CL) 
-figure(3);
-bodemag(U);
+%figure(3);
+%bodemag(U);
 
 
 %% Plots
 hold on
-
+figure(1)
 subplot(2,2,1)
 CL_old = tf(conv(T_old,G3.b), P_end_old, Ts,'variable','z^-1'); 
 CL = tf(conv(T,G3.b), P_end, Ts,'variable','z^-1'); 
 title("Tracking step response, original and improved controller")
-xlabel("Time [s]")
+xlabel( "Time [s]")
 legend("Original", "Improved")
-step(CL_old)
-step(CL)
+step(CL_old, CL)
+
 
 subplot(2,2,2)
 B = G3.B;
 B = B(2:end);
-input_old = tf(conv(T_old,A), conv(A,S_old) + conv(conv(q_delay, B), R_old), Ts,'variable','z^-1'); figure(1);  
-input= tf(conv(T,A), conv(A,S_final) + conv(conv(q_delay, B), R_final), Ts,'variable','z^-1'); figure(1);  
+
+
 title("Control signal, original and improved controller")
 xlabel("Time [s]")
 ylabel("u")
 legend("Original", "Improved")
-step(input_old)
-step(input)
+step(input_old, input)
+
 
 subplot(2,2,3)
 Sens_out = feedback(1,K*G3);
@@ -144,4 +148,14 @@ Sens_out_new = feedback(1,K_final*G3);
 MM_old = norm(Sens_out, Inf)^-1
 MM_new = (norm(Sens_out_new, Inf))^(-1)
 
+title("Output sensitivity function, original and improved controller")
+legend("Original","Improved")
+bodemag(Sens_out, Sens_out_new)
+
+
 subplot(2,2,4)
+U=tf(conv(A,R), P_end_old, Ts, 'variable', 'z^-1');
+U_new=tf(conv(A,R_final), P_end, Ts, 'variable', 'z^-1');
+title("Input sensitivity function, original and improved controller")
+legend("Original","Improved")
+bodemag(U, U_new)
